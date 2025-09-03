7bb21bb5017a40ea8fb53f7054856087f358aa59
muntory <38274669+muntory@users.noreply.github.com>
2025-09-03 12:36:45 +0900

    [fix] 맵 타일맵 수정, 맵 재배치 로직 수정

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Stage : MonoBehaviour
 {
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (!collision.gameObject.CompareTag("Player"))
-        {
-            return;
-        }
-
-        Debug.Log("¸Ê Àç¹èÄ¡");
-
-        transform.position = collision.transform.position;
-    }
+    
+    
 }
